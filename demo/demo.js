ce412ca1f3c7c22dba791dcd4fe3e82f8719b592
Christopher Guthrie <christopher.guthrie@blackrock.com>
2016-02-19 18:34:09 -0500

    Added empty readme

--- demo/demo.js
+++ demo/demo.js
@@ -1,12 +1,19 @@
 const sliderDemo = angular.module('slider-demo', ['angular-slider-histogram'])
 
-.controller('demoCtrl', function($scope, $element){
+.controller('demoCtrl', function($scope, $element, $http){
     $scope.slider = {
         minValue: 20,
-        maxValue: 80,
+        maxValue: 50,
         options: {
             floor: 0,
             ceil: 100
         }
     };
+
+    $http.get("demoData.json").then(function(data){
+        $scope.series = _.map(data.data, function(point){
+            return parseFloat(point.value);
+        });
+        $scope.ready = true;
+    });
 });
--- src/directives/sliderHistogram.js
+++ src/directives/sliderHistogram.js
@@ -2,14 +2,34 @@ angular.module('angular-slider-histogram').directive('sliderHistogram', function
     return {
         restrict: 'E',
         scope: {
-            slider: '='
+            slider: '=',
+            series: '=',
+            bucketCount: '=?'
         },
         controller: function($scope, $element){
-            $scope.chart = new Highcharts.Chart({
+            // Default buckets to 25
+            $scope.bucketCount = $scope.bucketCount || 25;
+        },
+        link: function(scope, element){
+            scope.$watch("slider.minValue", function(newVal, oldVal){
+                if( scope.chart && typeof newVal == "number" && typeof oldVal == "number"){
+                    paintBuckets(scope.slider.minValue, scope.slider.maxValue);
+                }
+            });
+            scope.$watch("slider.maxValue", function(newVal, oldVal){
+                if( scope.chart && typeof newVal == "number" && typeof oldVal == "number"){
+                    paintBuckets(scope.slider.minValue, scope.slider.maxValue);
+                }
+            });
+            scope.chart = new Highcharts.Chart({
                 chart: {
                     type: 'column',
-                    renderTo: $element.find(".slider-histogram"),
-                    animation: false
+                    renderTo: element.find(".slider-histogram").get(0),
+                    animation: false,
+                    spacingLeft: 0,
+                    spacingRight: 0,
+                    height: element.parent().height(),
+                    width: element.parent().width()
                 },
                 exporting: {
                     enabled: false
@@ -31,20 +51,61 @@ angular.module('angular-slider-histogram').directive('sliderHistogram', function
                 credits: {
                     enabled: false
                 },
+                series: [{
+                    data: groupData(scope.series),
+                    color: 'lightgrey'
+                }],
+                title: {
+                    text: null
+                },
                 xAxis: {
                     title: {
                         text: null
-                    }
+                    },
+                    labels: {
+                        enabled: false
+                    },
+                    visible: false
                 },
                 yAxis: {
                     title: {
                         text: null
-                    }
+                    },
+                    gridLineWidth: 0,
+                    labels: {
+                        enabled: false
+                    },
+                    visible: false
                 }
             });
-        },
-        link: function(scope, element){
 
+            function paintBuckets(fromBucket, toBucket){
+                const bucketInterval = scope.slider.options.ceil / scope.bucketCount;
+                const fromData = parseInt(fromBucket/bucketInterval) - 1;
+                const toData = parseInt(toBucket/bucketInterval) - 1;
+                $.each(scope.chart.series[0].data, function(index, datum){
+                    if( index >= fromData && index <= toData) {
+                        datum.update({
+                            color: 'lightgrey'
+                        });
+                    }
+                    else{
+                        datum.update({
+                            color: '#ededed'
+                        });
+                    }
+                });
+            }
+
+            function groupData(data){
+                var buckets =  Array.apply(null, Array(scope.bucketCount)).map(Number.prototype.valueOf,0);
+                $.each(data, function(index, point){
+                    buckets[parseInt(point / (scope.slider.options.ceil / scope.bucketCount))]++;
+                });
+                return buckets;
+            }
+
+            element.find(".rz-bubble").css("top", "18px");
         },
         templateUrl: '../src/directives/templates/sliderHistogram.html'
     }
